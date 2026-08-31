6cc9978dcbfb0bbe47de6631b8b19cf6e4464227
Aleesha Ali <aleeshaali048@gmail.com>
2026-08-31 14:14:53 +0500

    Fix sentiment service dependencies and frontend config URL

--- giftlink-backend/util/import-mongo/index.js
+++ giftlink-backend/util/import-mongo/index.js
@@ -2,49 +2,37 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+let url = "mongodb://127.0.0.1:27017";
 let filename = `${__dirname}/gifts.json`;
 const dbName = 'giftdb';
 const collectionName = 'gifts';
 
-// notice you have to load the array of gifts into the data object
-const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs;
-
-// connect to database and insert data into the collection
 async function loadData() {
     const client = new MongoClient(url);
 
     try {
-        // Connect to the MongoDB client
         await client.connect();
         console.log("Connected successfully to server");
 
-        // database will be created if it does not exist
         const db = client.db(dbName);
-
-        // collection will be created if it does not exist
         const collection = db.collection(collectionName);
-        let cursor = await collection.find({});
-        let documents = await cursor.toArray();
 
-        if(documents.length == 0) {
-            // Insert data into the collection
-            const insertResult = await collection.insertMany(data);
-            console.log('Inserted documents:', insertResult.insertedCount);
-        } else {
-            console.log("Gifts already exists in DB")
-        }
+        let rawData = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+        // Handle both direct array or nested array objects (e.g. { docs: [...] } or { gifts: [...] })
+        let data = Array.isArray(rawData) 
+            ? rawData 
+            : (rawData.docs || rawData.gifts || Object.values(rawData)[0]);
+
+        await collection.deleteMany({});
+
+        const insertResult = await collection.insertMany(data);
+        console.log(`Inserted documents: ${insertResult.insertedCount}`);
     } catch (err) {
-        console.error(err);
+        console.error("Error connecting or inserting data:", err);
     } finally {
-        // Close the connection
         await client.close();
     }
 }
 
 loadData();
-
-module.exports = {
-    loadData,
-  };
